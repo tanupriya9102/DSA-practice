aecb2228af40ac42d6adae18f9cbc3ee1368aaa9
Tanupriya Pathak <pathak.tanupriya@gmail.com>
2023-06-12 15:19:21 +0530

    commit

--- BST/Traversals.py
+++ BST/Traversals.py
@@ -28,6 +28,21 @@ def preOrder(root):
     preOrder(root.left)
     preOrder(root.right)
 
+def inOrder(root):
+    if root is None:
+        return
+    inOrder(root.left)
+    print(root.val)
+    inOrder(root.right)
+
+def postOrder(root):
+    if root is None:
+        return
+    postOrder(root.left)
+    postOrder(root.right)
+    print(root.val)
+
+
 
 
 root=BSTNode(10)
@@ -35,8 +50,8 @@ insertNode(root,2)
 insertNode(root,12)
 insertNode(root,1)
 insertNode(root,4)
-insertNode(root,10)
+insertNode(root,11)
 # insertNode(root,10)
 insertNode(root,14)
 # print(root.val)
-preOrder(root)
+inOrder(root)
