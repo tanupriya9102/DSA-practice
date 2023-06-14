76dde6fc79ee56aad3d686a7711476e4ebf05697
Tanupriya Pathak <pathak.tanupriya@gmail.com>
2023-06-14 10:42:22 +0530

    commit

--- Heap/HeapArray.py
+++ Heap/HeapArray.py
@@ -24,6 +24,37 @@ def levelOrder(root):
             print(root.customList[i])
 
 
+def heapifyInsert(root,index,heapType):
+    parent=int(index/2)
+    if index<=1:
+        return
+    if heapType=='min':
+        if root.customList[index]<root.customList[parent]:
+            temp=root.customList[index]
+            root.customList[index]=root.customList[parent]
+            root.customList[parent]=temp
+        heapifyInsert(root,parent,heapType)
+    elif heapType=='max':
+        if root.customList[index]>root.customList[parent]:
+            temp=root.customList[index]
+            root.customList[index]=root.customList[parent]
+            root.customList[parent]=temp
+        heapifyInsert(root,parent,heapType)
 
-Bheap=Heap(5)
-print(sizeHeap(Bheap))
+def Insert(root,value,heapType):
+    if root.heapSize +1==root.maxSize:
+        return "The heap is full!!"
+    root.customList[root.heapSize +1]=value
+    root.heapSize+=1
+    heapifyInsert(root,root.heapSize,heapType)
+    return "Value Inserted!"
+Bheap=Heap(10)
+# print(sizeHeap(Bheap))
+Insert(Bheap,4,'max')
+Insert(Bheap,5,'max')
+Insert(Bheap,2,'max')
+Insert(Bheap,1,'max')
+Insert(Bheap,6,'max')
+# Insert(Bheap,8,'max')
+Insert(Bheap,0,'max')
+levelOrder(Bheap)
