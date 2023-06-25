7b263304f7c6aec499747023a707c97469a53aff
tanupriya9102 <pathak.tanupriya@gmail.com>
2023-06-25 11:00:07 +0530

    commit

--- graphs/graphDict.py
+++ graphs/graphDict.py
@@ -14,6 +14,16 @@ class Graph:
     def addVertex(self,vertex):
         self.gdict[vertex]=[]
 
+    def delEdge(self,vertex,edge):
+        self.gdict[vertex].remove(edge)
+    def delVertex(self,vertex):
+        del self.gdict[vertex]
+        for i in self.gdict:
+            if vertex in self.gdict[i]:
+                self.gdict[i].remove(vertex)
+
+  
+
 d={'a':['b','c'],
    'b':['d','e'],
    'c':['a','e'],
@@ -22,11 +32,15 @@ d={'a':['b','c'],
    }
 
 graph=Graph(d)
-print(graph.gdict)
+# print(graph.gdict)
 graph.addVertex('f')
-print(graph.gdict)
+# print(graph.gdict)
 graph.addEdge('f','d')
-print(graph.gdict)
+# print(graph.gdict)
 graph.addEdge('f','e')
 print(graph.gdict)
-
+print('---------------------------------------------')
+# graph.delEdge('f','e')
+# print(graph.gdict)
+graph.delVertex('f')
+print(graph.gdict)
