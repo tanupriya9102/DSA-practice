f4215e72140be3c34f3069624ebc7c0d02501ccf
tanupriya9102 <pathak.tanupriya@gmail.com>
2024-03-28 14:45:11 +0530

    fibMemo

---
+++ DP/fibonaccimemo.py
@@ -0,0 +1,10 @@
+def fibMemo(n,memo):
+    if n==1:
+        return 0
+    if n==2:
+        return 1
+    if n not in memo:
+        memo[n]=fibMemo(n-1,memo)+fibMemo(n-2,memo)
+    return memo[n]
+
+print(fibMemo(6,{}))
